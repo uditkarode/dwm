c77bd01c44ac9fda99f41d1f84ac621f0d34c338
Udit Karode <udit.karode@gmail.com>
2020-07-30 21:04:23 +0530

    dwm: change some color schemes
    * ocd acting up again :<

--- dwm.c
+++ dwm.c
@@ -892,7 +892,7 @@ drawbar(Monitor *m)
 		continue;
 
 		w = TEXTW(tags[i]);
-		drw_setscheme(drw, scheme[m->tagset[m->seltags] & 1 << i ? SchemeGreen : SchemeSel]);
+		drw_setscheme(drw, scheme[m->tagset[m->seltags] & 1 << i ? SchemeGreen : SchemeCyan]);
 		drw_text(drw, x, 0, w, bh, lrpad / 2, tags[i], urg & 1 << i);
 		x += w;
 	}
